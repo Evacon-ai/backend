df65db10667ff54abadbc5b8b554ee79b20fc8bd
Alex Zehnbacht <alex.business@gmail.com>
2025-04-22 14:26:28 -0700

    users accessible to everyone

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,11 +9,10 @@ router.all("*", verifyAuth);
 // Routes that any authenticated user can access
 router.get("/me", userController.getCurrentUser);
 
-// Admin-only routes
-router.get("/", requireAdmin, userController.getAllUsers);
-router.get("/:id", requireAdmin, userController.getUserById);
-router.post("/", requireAdmin, userController.createUser);
-router.put("/:id", requireAdmin, userController.updateUser);
-router.delete("/:id", requireAdmin, userController.deleteUser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
