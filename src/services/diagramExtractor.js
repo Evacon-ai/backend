c5e8424e7134d93411aeeec10ff4c4dd5e4d0192
Alex Zehnbacht <alex.business@gmail.com>
2025-05-16 20:55:27 -0700

    added signing files

--- src/services/diagramExtractor.js
+++ src/services/diagramExtractor.js
@@ -1,14 +1,40 @@
 const axios = require("../utils/axios");
+const { Storage } = require("@google-cloud/storage");
 
 async function extractElementsFromDiagram(diagramUrl) {
   if (!diagramUrl) {
     throw new Error("Missing diagram URL");
   }
 
+  // Generate signed URL for the diagram
+
+  var serviceAccount;
+  if (process.env.STORAGE_SECRET)
+    serviceAccount = JSON.parse(process.env.STORAGE_SECRET);
+  else
+    serviceAccount = require("../config/secret/evacon-ai-565c5-d4e3a89d59d4.json");
+
+  const storage = new Storage({
+    projectId: "evacon-ai-565c5",
+    keyFilename: serviceAccount,
+  });
+
+  const filePath = extractFirebaseStoragePath(diagramUrl);
+
+  const [url] = await storage
+    .bucket("evacon-ai-565c5.appspot.com")
+    .file(filePath)
+    .getSignedUrl({
+      action: "read",
+      expires: Date.now() + 15 * 60 * 1000, // 15 min
+    });
+
+  console.log("Signed URL:", url);
+
   try {
     const response = await axios.post(
       "https://evacon-extractor-754396764509.us-central1.run.app/extract_ic",
-      { url: diagramUrl },
+      { url: url },
       { timeout: 10000 }
     );
 
@@ -23,6 +49,19 @@ async function extractElementsFromDiagram(diagramUrl) {
     const genericError = new Error("Failed to extract diagram elements");
     genericError.statusCode = 500;
     throw genericError;
+  }
+}
+
+function extractFirebaseStoragePath(url) {
+  try {
+    const match = url.match(/\/o\/(.*?)\?/);
+    if (!match || !match[1]) {
+      throw new Error("Invalid Firebase Storage URL");
+    }
+    return decodeURIComponent(match[1]);
+  } catch (err) {
+    console.error("Failed to extract path:", err.message);
+    return null;
   }
 }
 
